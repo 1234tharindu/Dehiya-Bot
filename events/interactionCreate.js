c9d379211199ee0d22006e6a56910fc59492f95f
Chirath Timodh <chirathtimodh@gmail.com>
2023-02-16 14:35:03 +0530

    make events more order

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,4 +1,6 @@
 const { Events } = require('discord.js');
+const utils = require('../utils/ticket.js');
+
 module.exports = {
     name: Events.InteractionCreate,
     once: false,
@@ -14,6 +16,7 @@ module.exports = {
                 console.error(error);
                 await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
             };
+
         } else if (interaction.isAutocomplete()) {
 
             if (!command) return;
@@ -22,6 +25,33 @@ module.exports = {
                 await command.autocomplete(interaction);
             } catch (error) {
                 console.error(error);
+            }
+
+        } else if (interaction.isButton()) {
+            switch (interaction.customId) {
+
+                case 'createTicket':
+                    utils.createTicket(interaction, client);
+                    break;
+
+                case 'closeConfirmation':
+                    utils.closeConfirmation(interaction);
+                    break;
+
+                case 'closeCancel':
+                    utils.closeCancel(interaction);
+                    break;
+                case 'closeTicket':
+                    utils.closeTicket(interaction, client);
+                    break;
+
+                case 'openTicket':
+                    utils.openTicket(interaction);
+                    break;
+
+                case 'deleteTicket':
+                    utils.deleteTicket(interaction, client);
+                    break;
             }
         }
     }
