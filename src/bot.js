edf8c85297863908820529e1f2abaa84728ec6ab
Chirath Timodh <chirathtimodh@gmail.com>
2023-02-20 18:16:03 +0530

    added connection to host

--- src/bot.js
+++ src/bot.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => res.send("bot online yay boy!!"));
+
+app.listen(port, () =>
+    console.log(`Your app is listening a http://localhost:${port}`)
+);
+
 require('dotenv').config();
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const { readdirSync } = require('fs');
