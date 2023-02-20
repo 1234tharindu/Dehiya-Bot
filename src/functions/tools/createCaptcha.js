d194a67f6b1ef91f6cdddd51a36689e2c7a0e27b
Chirath Timodh <chirathtimodh@gmail.com>
2023-02-20 19:38:08 +0530

    fixed

--- src/functions/tools/createCaptcha.js
+++ src/functions/tools/createCaptcha.js
@@ -1,3 +1,12 @@
+const shuffle = (arr) => {
+    let i = arr.length;
+    while (i) {
+        let j = Math.floor(Math.random() * i--);
+        [arr[j], arr[i]] = [arr[i], arr[j]];
+    }
+    return arr;
+};
+
 module.exports = async function createCaptcha(length = 6, blacklist = "") {
 
     let chars = [
